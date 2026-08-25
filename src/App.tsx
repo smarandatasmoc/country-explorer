e88b1db67dec3b8135eb4e6db542341f5569713f
Smaranda Tasmoc <smaranda_tasmoc@MacBook-Air-2.fritz.box>
2026-08-25 13:25:40 +0300

    Fixes #1

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,27 @@ import {
   BrowserRouter,
   Routes,
   Route,
+  Navigate,
 } from 'react-router-dom'
 
 import Auth from './routes/Auth'
 import Country from './routes/Country'
 import Profile from './routes/Profile'
+import Home from './routes/Home'
 import ProtectedRoute from './hooks/ProtectedRoute'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route 
+          path="/" 
+          element={<Navigate to="/home" replace />} 
+        />
+        <Route 
+          path="/home" 
+          element={<Home/>} 
+        />
         <Route path="/login" element={<Auth />} />
 
         <Route
